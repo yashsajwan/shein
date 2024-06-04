5299656abd0970195613a61ca6c8134d2fa6b42b
yashsajwan <yash.sajwan@buildwithinnovation.com>
2024-06-04 20:55:09 +0530

    refer and earn updted

--- components/advertisement/Advertisement.tsx
+++ components/advertisement/Advertisement.tsx
@@ -2,33 +2,22 @@
 import React from "react";
 import Image from "next/image";
 // import advestisingImg from "../../images/Group 34289.png";
-import advertise1 from "../../images/advertise1.jpg";
-import advertise2 from "../../images/advertise2.jpg";
+import referandearn from "../../images/referandearn.png";
 
 const Advertisement = () => {
   return (
     <>
-      <div className=" w-full mt-0 px-[1.5%]    z-30 ">
-        <div className="  w-full  flex  items-center sm:gap-2  ">
-          <div className="">
-            <Image
-              src={advertise1}
-              alt=""
-              width={1000}
-              height={1000}
-              className="flex-1 lg:h-[25rem] md:h-[15rem] h-[10rem] object-cover "
-            />
-          </div>
-          <div className=" ">
-            <Image
-              src={advertise2}
-              alt=""
-              width={1000}
-              height={1000}
-              className="flex-1 lg:h-[25rem] md:h-[15rem] h-[10rem] object-cover "
-            />
-          </div>
-        </div>
+      <div className=" w-full mt-0 px-[1.5%] relative">
+        {/* <div className="bottom-[20%] right-[20%] absolute text-white bg-black px-3 py-2 rounded-full">
+        https://shein-amber.vercel.app/
+        </div> */}
+        <Image
+          src={referandearn}
+          alt=""
+          width={1000}
+          height={1000}
+          className="object-cover w-full"
+        />
       </div>
     </>
   );
